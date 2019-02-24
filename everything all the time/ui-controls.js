6ac20d27f07c7ff1d532971b4db4ae4a9fc798a2
Valiauga Lukas <valiaugalukas@gmail.com>
2019-02-24 13:36:09 +0000

    Add files via upload

---
+++ everything all the time/ui-controls.js
@@ -0,0 +1,46 @@
+//$('body').hide();        
+var loader = $('#loader');
+
+$('#canvas-ui').css('opacity',0);
+//$('arrow_down').hide();
+$('body').prepend(loader);
+
+$('#canvas-ui').fadeTo(0,0);
+$(document).ready(function(){
+    $(this).delay(2000);
+    console.log("ready");
+    $('#canvas-ui').fadeTo(2000,1);    
+    //$('body').show();    
+    //    $('body').fadeIn(1000);
+    
+//    $('body').css("opacity",1);    
+    //    $('card').prop('disabled', true);
+
+    $('h2-hover').hover(function(){
+        console.log("card hover");
+        $('h2-hover').fadeTo('slow',0.1);
+    });
+
+    $("#arrow_up").click(function() {
+        console.log("arrow click");
+        var offset = 80;
+        $('html, body').animate({
+        scrollTop: $("#about").offset().top + offset
+        }, 1000);
+    });
+
+
+    //back to the top
+    $('#arrow_bottom').click(function(){
+        console.log("arrow_bottom");
+        $('html,body').animate({scrollTop: document.body.scrollHeight},"slow"); 
+    });
+
+    var sH;
+    $(window).scroll(function(){
+        var content_canvas = $('.padding-100').css("opacity");
+        sH = $(this).scrollTop();
+        $('.padding-100').css("opacity",Math.max(0.001,sH/$(window).height())); 
+        $('#arrow_up').css("opacity",Math.max(0.001,1-sH/$(window).height())); 
+    });
+});
