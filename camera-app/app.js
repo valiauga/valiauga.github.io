0c1a7176324eb678f3ba2de1f64c7380343c7aea
Lukas Valiauga <valiaugalukas@gmail.com>
2020-04-18 13:46:23 +0200

    Add files via upload

---
+++ camera-app/app.js
@@ -0,0 +1,32 @@
+let constraints = { 
+    video: { facingMode: "environment" }, //"user" -> front // "environment" -> rear
+    audio: false 
+};
+
+const cameraView = document.querySelector("#camera--view"),
+      cameraOutput = document.querySelector("#camera--output"),
+      cameraSensor = document.querySelector("#camera--sensor"),
+      cameraTrigger = document.querySelector("#camera--trigger")
+
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
