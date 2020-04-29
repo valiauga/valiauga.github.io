16195c21533218e9cd4220ee7ea7651c3d332586
Lukas Valiauga <valiaugalukas@gmail.com>
2020-04-29 09:15:53 +0200

    Add files via upload

--- video_cam/main.js
+++ video_cam/main.js
@@ -20,7 +20,15 @@ let objList = [''];
 let svg_num =1;
 let base_img;
 
+$( document ).ready(function() {
+    $("#camera--trigger").html(prompt[Math.round(Math.random(prompt.length)*10)]);
+    // $( '#camera--overlay' ).attr("src","svg/shape-0" + Math.floor(Math.random(100)*10) + ".svg");console.log(Math.floor(Math.random(10)*10));
+    $( '#camera--overlay' ).attr("src","svg/shape-0" + Math.floor(Math.random(1,9)*10)+ ".svg");
+});
+
+const b_next = document.getElementById('camera--next');
 const button = document.querySelector('button');
+
 button.onclick = function() {
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
@@ -31,21 +39,33 @@ button.onclick = function() {
     canvas.height = video.videoHeight;
     canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    $( '#camera--overlay' ).attr("src","svg/shape-0" + svg_num + ".svg");
-    console.log(svg_num);
-    svg_num +=1;
-    if (svg_num > 9) {
-      svg_num = 1;
-    }
-
-    $("#camera--trigger").html(prompt[Math.round(Math.random(prompt.length)*10)]);
-    console.log("prompt " + Math.round(Math.random(prompt.length)*10));
 
     // console.log(canvas.toDataURL());
     // base_img = canvas.toDataURL();
     // $('#the--image').attr("href", base_img);
 
 }//BTN...
+
+
+b_next.onclick = function() {
+
+  $( '#camera--overlay' ).attr("src","svg/shape-0" + svg_num + ".svg");
+  console.log(svg_num);
+  svg_num +=1;
+  if (svg_num > 9) {
+    svg_num = 1;
+  }
+
+  canvas.width = 0;
+  canvas.height = 0;
+
+  $("#camera--trigger").html(prompt[Math.round(Math.random(prompt.length)*10)]);
+  console.log("prompt " + Math.round(Math.random(prompt.length)*10));
+
+
+}
+
+
 
 // Camera handling...
 const constraints = {
@@ -62,3 +82,7 @@ function handleError(error) {
   console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
 }
 navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+
+// document.getElementById('clear').addEventListener('click', function() {
+//         context.clearRect(0, 0, canvas.width, canvas.height);
+//       }, false);
